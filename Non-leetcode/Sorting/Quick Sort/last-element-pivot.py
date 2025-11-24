14eb65d96eb5c8850788bb96a2c63fd248eaa9c6
chandarvijay23 <chandarvijay23@gmail.com>
2025-11-24 06:03:04 +0000

    Day 13

---
+++ Non-leetcode/Sorting/Quick Sort/last-element-pivot.py
@@ -0,0 +1,15 @@
+class Sort:
+    def quickSort(self, array):
+        if len(array) <= 1:
+            return array
+        
+        pivot_element = array[-1]
+        less_than = [i for i in array if i < pivot_element]
+        equal_to = [i for i in array if i == pivot_element]
+        greater_than = [i for i in array if i > pivot_element]
+
+        return self.quickSort(less_than) + equal_to + self.quickSort(greater_than)
+
+# Test
+nums = [5, 2, 8, 1, 3]
+print(Sort().quickSort(nums))
---
+++ Non-leetcode/Sorting/Quick Sort/middle-element-pivot.py
@@ -0,0 +1,33 @@
+# class Sort:
+#     def quickSort(self, array):
+#         if len(array) <= 1:
+#             return array
+        
+#         pivot_element = array[len(array) // 2]
+#         less_than = [i for i in array if i < pivot_element]
+#         equal_to = [i for i in array if i == pivot_element]
+#         greater_than = [i for i in array if i > pivot_element]
+
+#         return self.quickSort(less_than) + equal_to + self.quickSort(greater_than)
+
+# # Test
+# nums = [5, 2, 8, 1, 3]
+# print(Sort().quickSort(nums))
+
+
+class Sort:
+    @staticmethod
+    def quickSort(array):
+        if len(array) <= 1:
+            return array
+        
+        pivot_element = array[len(array) // 2]
+        less_than = [i for i in array if i < pivot_element]
+        equal_to = [i for i in array if i == pivot_element]
+        greater_than = [i for i in array if i > pivot_element]
+
+        return Sort.quickSort(less_than) + equal_to + Sort.quickSort(greater_than)
+
+# Test
+nums = [5, 2, 8, 1, 3]
+print(Sort.quickSort(nums))
