f7c33e8ec5b929f248af29d2dac8307f379c7373
chandarvijay23 <chandarvijay23@gmail.com>
2025-11-25 06:51:11 +0000

    Day 14

---
+++ Sliding Window/Best Time to Buy and Sell Stock/two-pointer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maximum = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maximum = max(maximum, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        return maximum
